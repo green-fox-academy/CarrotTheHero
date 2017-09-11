90048dd82f68957bb580ba2388f20e14261a62e1
Balazs Bognar <bali.bognar@outlook.com>
2017-09-11 17:33:25 +0200

    23-24 but 25 is 29 not 25 and not done yet

---
+++ week-02/day-01/25Task/25Task/Program29.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            Console.Write("write down a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= number; i++)
+                Console.WriteLine("*");
+
+            Console.ReadLine();
+        }
+    }
+}
