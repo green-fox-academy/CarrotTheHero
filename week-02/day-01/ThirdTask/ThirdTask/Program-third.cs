77f7d854a73e957205bd6f5837031543984c40b2
Balazs Bognar <bali.bognar@outlook.com>
2017-09-11 13:40:33 +0200

    exercise 1-3

---
+++ week-02/day-01/ThirdTask/ThirdTask/Program-third.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Gergo!");
+            Console.WriteLine("Hello, Flora!");
+            Console.WriteLine("Hello, Evi!");
+            Console.ReadLine();
+        }
+    }
+}
