d6b469dc0750faa883941a768aa0facf499cbc8f
Balazs Bognar <bali.bognar@outlook.com>
2017-09-12 17:13:00 +0200

    Task1-4

---
+++ week-02/day-02/Task4/Task4/Program4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+
+            int num = 42;
+            Console.WriteLine(Sum(num));
+            Console.ReadLine();
+
+        }
+
+        public static int Sum(int input)
+        {
+            for (int i = input; i > 0; i--)
+            {
+                input += i - 1;
+            }
+            return input;
+
+        }
+    }
+}
