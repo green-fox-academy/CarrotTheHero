aa6ea06aae159b07c173bbd9e12fe4979bf58e93
Balazs Bognar <bali.bognar@outlook.com>
2017-09-13 16:32:39 +0200

    Task 9-12

--- week-02/day-03/Task7/Task7/Program7.cs
+++ week-02/day-03/Task7/Task7/Program7.cs
@@ -15,7 +15,19 @@ namespace Matchmaking
             // Join the two lists by matching one girl with one boy in the order list
             // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
 
+            for (int i = 0; i < boys.Count; i++)
+            {
+                if (girls.Count > i)
+                {
+                    order.Add(girls[i]);
+                }
+                if (boys.Count > i)
+                {
+                    order.Add(boys[i]);
+                }
+            }
             
+
             foreach (string person in order)
             {
                 Console.WriteLine(person);
