f9be57014bcced095a958bb5d35613d46b580830
Balazs Bognar <bali.bognar@outlook.com>
2017-09-14 16:47:03 +0200

    Task 31-35

--- week-02/day-01/Task32/Task32/Program32.cs
+++ week-02/day-01/Task32/Task32/Program32.cs
@@ -29,13 +29,18 @@ namespace GreenFox
 
             Console.WriteLine();
 
-            for (int i = 0; i < num-2; i++)
+            for (int i = 0; i < num - 2; i++)
             {
                 Console.Write("%");
 
                 for (int j = 0; j < num - 2; j++)
                 {
-                    Console.Write(" ");
+                    if (i == j)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                        Console.Write(" ");
                 }
 
                 Console.Write("%");
---
+++ week-02/day-01/Task34/Task34/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Write in a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Write in as many numbers as you said before: ");
+            int secnum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                secnum += Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Sum: " + secnum + ", Average: " + (secnum / num));
+            Console.ReadLine();
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+        }
+    }
+}
