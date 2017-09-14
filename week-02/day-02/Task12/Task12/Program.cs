ac12480f0c8f3f927d8ee54e7fd3d1d32372f87a
Balazs Bognar <bali.bognar@outlook.com>
2017-09-14 17:34:13 +0200

    Task 11-13 but 13 is not done yet

---
+++ week-02/day-02/Task12/Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = new int[] { 4, 5, 6, 7 };
+
+            foreach (int word in af)
+            {
+                Console.WriteLine(word);
+            }
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-02/Task13/Task13/Program13.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[4, 4];
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                foreach (int things in matrix)
+                {
+                    Console.WriteLine(things);
+                }
+            }
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+            Console.ReadLine();
+        }
+    }
+}
