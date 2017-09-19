f455cfe48ad119204175cf6fb6fc3f2763f59c3b
Balazs Bognar <bali.bognar@outlook.com>
2017-09-19 14:02:21 +0200

    Task5-6 done

--- week-03/day-02/Task5/Task5/Program.cs
+++ week-03/day-02/Task5/Task5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace Task5
     {
         static void Main(string[] args)
         {
+            // Create a function that takes 3 parameters: a path, a word and a number,
+            // than it should write to a file.
+            // The path parameter should be a string, that describes the location of the file.
+            // The word parameter should be a string, that will be written to the file as lines
+            // The number paramter should describe how many lines the file should have.
+            // So if the word is "apple" and the number is 5, than it should write 5 lines
+            // to the file and each line should be "apple"
+            // The function should not raise any error if it could not write the file.
+
+            string path = "text.txt";
+            string word = "coffee";
+            int num = 5;
+            LineWriter(path, word, num);
+        }
+
+        private static void LineWriter(string path, string word, int num)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    writer.WriteLine(word);
+                }
+            }
         }
     }
 }
---
+++ week-03/day-02/Task6/Task6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
+            string path = "text.txt";
+            string outtext = "texttwo.txt";
+
+            Textcopy(path, outtext);
+            Console.ReadLine();
+
+        }
+
+        private static void Textcopy(string path, string outtext)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string[] content = File.ReadAllLines(path);
+
+                    using (StreamWriter writer = new StreamWriter(outtext))
+                    {
+                        for (int i = 0; i < content.Length; i++)
+                        {
+                            writer.WriteLine(content[i]);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Can't do it");
+            }
+        }
+    }
+}
