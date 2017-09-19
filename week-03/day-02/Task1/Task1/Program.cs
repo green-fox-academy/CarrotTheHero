4bd4fb28d0f2dc34e28410f9eebd0c3e36f5b3a4
Balazs Bognar <bali.bognar@outlook.com>
2017-09-19 11:21:26 +0200

    task 1 done

---
+++ week-03/day-02/Task1/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write down a number to divide 10 with it: ");
+            Divider();
+            Console.ReadLine();
+        }
+
+        private static void Divider()
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine (10 / input);
+            }
+            catch
+            {
+                Console.WriteLine("You can't divide it by 0");
+            }
+        }
+    }
+}
