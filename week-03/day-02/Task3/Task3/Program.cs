4eb288c8b4fde11ab46de9bf27c157510f8f356a
Balazs Bognar <bali.bognar@outlook.com>
2017-09-19 11:47:50 +0200

    task 3 done

--- week-03/day-02/Task3/Task3/Program.cs
+++ week-03/day-02/Task3/Task3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task3
 {
@@ -10,6 +11,22 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            string path = "text.txt";
+            Console.WriteLine(Lines(path));
+            Console.ReadLine();
+        }
+
+        private static int Lines(string path)
+        {
+            try
+            {
+                string[] content = File.ReadAllLines(path);
+                return content.Length;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
         }
     }
 }
