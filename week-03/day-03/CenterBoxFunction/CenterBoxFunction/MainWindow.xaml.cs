26da5326fc2cdb3a9db2909694e22d7c30bccc94
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 14:23:29 +0200

    Task 10 done

--- week-03/day-03/CenterBoxFunction/CenterBoxFunction/MainWindow.xaml.cs
+++ week-03/day-03/CenterBoxFunction/CenterBoxFunction/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using GreenFox;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CenterBoxFunction
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 1 parameter:
+            // the square size
+            // and draws a square of that size to the center of the canvas.
+            // draw 3 squares with that function.
+            DrawSquare(220);
+            DrawSquare(120);
+            DrawSquare(30);
+        }
+
+        private void DrawSquare(int size)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawRectangle(150 - (size / 2), 150 - (size / 2), size, size);
         }
     }
 }
---
+++ week-03/day-03/RainbowBoxFunction/RainbowBoxFunction/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace RainbowBoxFunction
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a square drawing function that takes 2 parameters:
+            // the square size, and the fill color,
+            // and draws a square of that size and color to the center of the canvas.
+            // create a loop that fills the canvas with rainbow colored squares.
+            //string color = foxDraw.FillColor(Colors.);
+            byte colnum = 10;
+            Byte[] color = new Byte[3] { colnum, colnum, colnum };
+            int size = 300;
+            SquareDraw(size, color, colnum);
+        }
+
+        private void SquareDraw(int size, byte[] color, byte colnum)
+        {
+            var foxDraw = new FoxDraw(canvas);
+
+            for (int i = 1; i < 30; i++)
+            {
+                colnum += Convert.ToByte(i * 2);
+                foxDraw.DrawRectangle(150 - (size- 10*i / 2), 150 - (size - 10*i / 2), size - 10*i, size - 10*i);
+                foxDraw.FillColor(Color.FromRgb(color[0], color[1], color[2]));
+            }
+        }
+    }
+}
