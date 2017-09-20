58cb693b8f581633a8464c5a44584baac1f55ffe
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 12:04:47 +0200

    task 6 done

---
+++ week-03/day-03/CenteredSquare/CenteredSquare/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace CenteredSquare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // draw a green 10x10 square to the canvas' center.
+            foxDraw.DrawRectangle(145, 145, 10, 10);
+            foxDraw.FillColor(Colors.Green);
+
+        }
+    }
+}
