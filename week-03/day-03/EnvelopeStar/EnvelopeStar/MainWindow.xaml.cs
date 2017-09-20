0b30d410b95415a464cd2e58f5e5bc6c1c6dee9f
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 17:01:54 +0200

    Task 17 done but just 85%

--- week-03/day-03/EnvelopeStar/EnvelopeStar/MainWindow.xaml.cs
+++ week-03/day-03/EnvelopeStar/EnvelopeStar/MainWindow.xaml.cs
@@ -14,28 +14,33 @@ namespace EnvelopeStar
             // reproduce this:
             // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/envelope-star/r2.png]
 
+
+            //top right
             for (int i = 0; i < 10; i++)
             {
                 foxDraw.StrokeColor(Colors.Lime);
-                foxDraw.DrawLine(150, 10 + i * 15, 160 + i * 15, 150);
+                foxDraw.DrawLine(150, 10 + i * 15, 150 + i * 15, 150);
             }
 
+            //bottom left
             for (int i = 0; i < 10; i++)
             {
                 foxDraw.StrokeColor(Colors.Lime);
-                foxDraw.DrawLine(150, 160 + i * 15, 10 + i * 15, 150);
+                foxDraw.DrawLine(150, 160 + i * 15, 0 + i * 15, 150);
             }
 
+            //top left
             for (int i = 0; i < 10; i++)
             {
                 foxDraw.StrokeColor(Colors.Lime);
-                foxDraw.DrawLine(150, 10 + i * 15, 160 - i * 15, 150);
+                foxDraw.DrawLine(150, 10 + i * 15, 150 - i * 15, 150);
             }
 
+            //bottom right
             for (int i = 0; i < 10; i++)
             {
                 foxDraw.StrokeColor(Colors.Lime);
-                foxDraw.DrawLine(150, 280 - i * 15, 160 + i * 15, 150);
+                foxDraw.DrawLine(150, 280 - i * 15, 150 + i * 15, 150);
             }
         }
     }
