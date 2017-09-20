5a2bb77b079e9b59efcf8875612691a40a1c95d5
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 15:41:56 +0200

    Task 14 done

---
+++ week-03/day-03/FunctionToCenter/FunctionToCenter/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace FunctionToCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // fill the canvas with lines from the edges, every 20 px, to the center.
+            
+            
+            for (int i = 0; i <= 300; i += 20)
+            {
+                LineDrawing(0, i);
+            }
+
+            for (int i = 0; i <= 300; i += 20)
+            {
+                LineDrawing(i, 0);
+            }
+
+            for (int i = 0; i <= 300; i += 20)
+            {
+                LineDrawing(i, 300);
+            }
+
+            for (int i = 0; i <= 300; i += 20)
+            {
+                LineDrawing(300, i);
+            }
+        }
+
+        private void LineDrawing(int x, int y)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawLine(x, y, canvas.Width / 2, canvas.Height / 2);
+            
+        }
+    }
+}
