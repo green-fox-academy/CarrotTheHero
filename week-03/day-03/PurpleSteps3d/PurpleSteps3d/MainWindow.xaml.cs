4ab0b9c019e7cf5c012a7a3beb13843a7510e60c
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 16:32:27 +0200

    Task 12 modified

--- week-03/day-03/PurpleSteps3d/PurpleSteps3d/MainWindow.xaml.cs
+++ week-03/day-03/PurpleSteps3d/PurpleSteps3d/MainWindow.xaml.cs
@@ -13,13 +13,15 @@ namespace PurpleSteps3d
             var foxDraw = new FoxDraw(canvas);
             // reproduce this:
             // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
-            double x = 5;
+            double x = 0;
+            double y = 10;
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 1; i < 6; i++)
             {
-                x *= 2;
+                y += 10;
+                x += i*10;
                 foxDraw.FillColor(Colors.Purple);
-                foxDraw.DrawRectangle(x, x, x, x);
+                foxDraw.DrawRectangle(x, x, y, y);
 
             }
         }
