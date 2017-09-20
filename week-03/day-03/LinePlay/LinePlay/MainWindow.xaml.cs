61ee93e03f383699b3a83ec810e1f363cc97e9ba
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 16:10:40 +0200

    Task 15 done

---
+++ week-03/day-03/LinePlay/LinePlay/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace LinePlay
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/line-play/r1.png]
+            int x = 0;
+            int y = 300;
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                foxDraw.StrokeColor(Colors.Lime);
+                foxDraw.DrawLine(0, 10 + i * 15, 10 + i * 15, 300);           
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                foxDraw.StrokeColor(Colors.MediumPurple);
+                foxDraw.DrawLine(10 + i * 15, 0, 300, 10 + i * 15);
+            }
+        }
+    }
+}
