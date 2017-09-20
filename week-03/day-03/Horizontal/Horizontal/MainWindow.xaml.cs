ade3006bcdda114e0c15e5441be86b9a416476dd
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 11:57:43 +0200

    task 5 done

--- week-03/day-03/Horizontal/Horizontal/MainWindow.xaml.cs
+++ week-03/day-03/Horizontal/Horizontal/MainWindow.xaml.cs
@@ -1,28 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using GreenFox;
 
 namespace Horizontal
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a 50 long horizontal line from that point.
+            // draw 3 lines with that function.
+            int x1 = 50;
+            int y1 = 100;
+
+            int x2 = 10;
+            int y2 = 70;
+
+            int x3 = 100;
+            int y3 = 200;
+
+            DrawLine(x1, y1);
+            DrawLine(x2, y2);
+            DrawLine(x3, y3);
+
+        }
+
+        private void DrawLine(int x, int y)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            foxDraw.DrawLine(x, y, x + 50, y);
         }
     }
 }
