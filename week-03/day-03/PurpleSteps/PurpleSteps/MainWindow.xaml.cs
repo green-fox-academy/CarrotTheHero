e6be0d7c28f3ab43fc113ffda2a82d062f757c75
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 14:47:21 +0200

    Task 11 done

---
+++ week-03/day-03/PurpleSteps/PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using GreenFox;
+using System.Windows.Media;
+
+namespace PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+            for (int i = 0; i < 15; i++)
+            {
+
+                foxDraw.FillColor(Colors.Purple);
+                foxDraw.DrawRectangle(0 + 15 * i, 0 + 15 * i, 15, 15);
+
+            }
+        }
+    }
+}
