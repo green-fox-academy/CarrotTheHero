314fcd4ef21b7bab2b034d7db2747be2845fe66d
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 11:50:38 +0200

    task 4

--- week-03/day-03/GoToCenter/GoToCenter/MainWindow.xaml.cs
+++ week-03/day-03/GoToCenter/GoToCenter/MainWindow.xaml.cs
@@ -15,7 +15,20 @@ namespace GoToCenter
             // the x and y coordinates of the line's starting point
             // and draws a line from that point to the center of the canvas.
             // draw 3 lines with that function.
+            Point point1 = new Point(10, 50);
+            Point point2 = new Point(100, 60);
+            Point point3 = new Point(0, 200);
+            LineToMiddle(point1);
+            LineToMiddle(point2);
+            LineToMiddle(point3);
 
+        }
+
+        private void LineToMiddle(Point point)
+        {
+            var foxDraw = new FoxDraw(canvas);
+            var middlepoint = new Point(150, 150);
+            foxDraw.DrawLine(point, middlepoint);                       
         }
     }
 }
