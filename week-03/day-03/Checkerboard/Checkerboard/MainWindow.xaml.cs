9ee864152b7620c55c198d976f63754462697b77
Balazs Bognar <bali.bognar@outlook.com>
2017-09-20 15:27:19 +0200

    Task 13 done

--- week-03/day-03/Checkerboard/Checkerboard/MainWindow.xaml.cs
+++ week-03/day-03/Checkerboard/Checkerboard/MainWindow.xaml.cs
@@ -1,28 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using GreenFox;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Checkerboard
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // fill the canvas with a checkerboard pattern.
+            int x = 0;
+            int y = 0;
+            foxDraw.FillColor(Colors.Black);
+
+            for (int i = 1; i <= 8; i++)
+            {                
+                for (int j = 0; j < 4; j++)
+                {
+                    foxDraw.DrawRectangle(x, y, 30, 30);
+                    x += 60;
+                }
+                if (i % 2 == 1)
+                    x = 30;
+                else
+                    x = 0;
+                    y += 30;
+                               
+            }
         }
     }
 }
