50d1ab4cad91bc2ecb8a62a5ef83d79aacd5c84b
Balazs Bognar <bali.bognar@outlook.com>
2017-09-21 13:10:31 +0200

    task 6 done

---
+++ week-03/day-04/Bunnies2/Bunnies2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunnies2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+            Console.WriteLine(Ears(7));
+            Console.ReadLine();
+        }
+
+        private static int Ears(int rabs)
+        {
+            if (rabs == 0)
+                return 0;
+            if (rabs % 2 == 1)
+                return 2 + Ears(rabs-1);
+            else return 3 + Ears(rabs-1);
+
+
+        }
+    }
+}
