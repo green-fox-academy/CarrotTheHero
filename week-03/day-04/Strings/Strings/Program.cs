e77327de1d7a9294ab1aacb49beae5ae9004ba42
Balazs Bognar <bali.bognar@outlook.com>
2017-09-21 14:09:57 +0200

    task 7 fixed by Norbi

--- week-03/day-04/Strings/Strings/Program.cs
+++ week-03/day-04/Strings/Strings/Program.cs
@@ -13,18 +13,32 @@ namespace Strings
             // Given a string, compute recursively (no loops) a new string where all the
             // lowercase 'x' chars have been changed to 'y' chars.
 
-            string input = "Xaaaxaxxxaaaxaaaxxxxa";
-            Console.WriteLine(XtoY(input));
+            Console.Write("Give me a string: ");
+            string input = Console.ReadLine();
+            int inputLength = input.Length;
+            Console.WriteLine((CharacterChange(input, inputLength)));
             Console.ReadLine();
         }
 
-        private static string XtoY(string input)
+        public static string CharacterChange(string input, int length)
         {
-            if (!input.Contains("x"))
-                return input;
-            else 
-            input = XtoY(input.Replace("x", "y"));
-            return input;
+            string result;
+            if (length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (input.Substring(length - 1, 1) == "x")
+                {
+                    result = "y";
+                    return CharacterChange(input, length - 1) + result;
+                }
+                else
+                {
+                    return CharacterChange(input, length - 1) + input.Substring(length - 1, 1);
+                }
+            }
         }
     }
 }
