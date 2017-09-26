494d4258e77e6e68de5ac9c0b714fd724817b292
Balazs Bognar <bali.bognar@outlook.com>
2017-09-26 17:30:58 +0200

    Task 1 PallidaClass soon done

---
+++ week-04/day-02/GreenFox/GreenFox/PallidaClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class PallidaClass
+    {        
+        private string className;
+        private List<Student> students;
+        private List<Mentor> mentors;
+
+        PallidaClass(string className)
+        {
+            this.className = className;
+            students = new List<Student>();
+            mentors = new List<Mentor>();
+
+        }
+
+        public void AddStudent(Student)
+        {
+            students.Add()
+        }
+
+        public void AddMentor(Mentor)
+        {
+            mentors.Add()
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Pallida " + className + " class has " + students.size + " students and " + mentors.size + " mentors.");
+        }
+    }
+}
