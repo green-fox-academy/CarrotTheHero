94615feb1c1b34f4c9f88ac03c076f8b278a9c99
Balazs Bognar <bali.bognar@outlook.com>
2017-09-27 15:19:44 +0200

    Anagram is done again

---
+++ week-04/day-03/Anagram/Anagram/Anag.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class Program
+    {        
+        public static bool Anagramma(string word1,string word2)
+        {
+                        
+                string word1letters = String.Concat(word1.OrderBy(c => c));
+                string word2letters = String.Concat(word2.OrderBy(c => c));
+
+            if (word1letters == word2letters)
+                return true;
+
+            else
+                return false;
+        }
+    }
+}
---
+++ week-04/day-03/Anagram/AnagramTest/AnagTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void PrintAnagram()
+        {
+            Program program = new Program();
+
+            Assert.AreEqual(true,Program.Anagramma("vajaskalacs","kavajacsals"));
+        }
+    }
+}
