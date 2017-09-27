76d0d4923b833847dc3c50d9f25f290b44bdb196
Balazs Bognar <bali.bognar@outlook.com>
2017-09-27 12:08:45 +0200

    apple is done

---
+++ week-04/day-03/Apples/Apples/Apples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplesApp
+{
+    public class Apples
+    {
+        public string input;
+        
+       public string GetApples()
+        {
+            string input = "apples";
+            return input;
+        }
+    }
+}
---
+++ week-04/day-03/Apples/ApplesTest/ApplesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ApplesApp;
+
+namespace ApplesTest
+{
+    [TestFixture]
+    public class ApplesTest
+    {
+        Apples apple = new Apples();
+        [Test]
+        public void PrintAppleTest()
+        {
+            string output = "apples";
+
+            Assert.AreEqual(output, apple.GetApples());
+        }
+    }
+}
