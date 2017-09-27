3a33d4a5d3ef3e21dbc91870cc2ded0590e25e13
Balazs Bognar <bali.bognar@outlook.com>
2017-09-27 13:47:26 +0200

    sum is done

---
+++ week-04/day-03/Sum/Sum/Summing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class Summing
+    {
+        public int Sum(List<int> SumList)
+        {
+            int sum = 0;
+
+            foreach (int number in SumList)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ week-04/day-03/Sum/SumTest/SummingTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture]
+    public class SummingTest
+    {
+        Summing sumtard = new Summing();
+
+        [Test]
+        public void PrintSumTest()
+        {
+            List<int> sumList = new List<int>{ 1, 2, 5, 6, 10, 17 };
+            int output = 41;
+
+            Assert.AreEqual(output, sumtard.Sum(sumList));
+        }
+    }
+}
