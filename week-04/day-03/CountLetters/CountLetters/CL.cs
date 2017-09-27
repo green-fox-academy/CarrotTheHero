35d7228c12e85df45aac1a4124cf036357540226
Balazs Bognar <bali.bognar@outlook.com>
2017-09-27 16:18:56 +0200

    CountLetters is done again

---
+++ week-04/day-03/CountLetters/CountLetters/CL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    public class CL
+    {
+        public Dictionary<char, int> MyFunction(string input)
+        {
+
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char letter in input)
+            {
+                if (!dictionary.ContainsKey(letter))
+                {
+                    dictionary.Add(letter, 1);
+                }
+                else
+                {
+
+                    dictionary[letter]++;
+                }
+            }
+            return dictionary;
+        }
+    }
+}
---
+++ week-04/day-03/CountLetters/CountLettersTest/CLT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CountLetters;
+
+namespace CountLettersTest
+{
+    [TestFixture]
+    public class CLT
+    {
+        [Test]
+
+        public void Count()
+        {
+            CL cl = new CL();
+            Dictionary<char, int> output = new Dictionary<char, int>()
+            {
+            {'t', 2 },
+            {'o', 3 },
+            {'r', 1 },
+            };
+
+            Assert.AreEqual(output, cl.MyFunction("totoro"));
+        }
+    }
+}
