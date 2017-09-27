9594d8e52a53f6e4ef0bfdd7ad7b39b2b46e81e7
Balazs Bognar <bali.bognar@outlook.com>
2017-09-27 16:50:20 +0200

    Fibonacci is done again

---
+++ week-04/day-03/Fibonacci/Fibonacci/Fibo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class Fibo
+    {
+        public double GetNthFibonacciWithFor(int n)
+        {
+            double nthFibonacci = 0;
+            if (n == 2)
+            {
+                nthFibonacci = 1;
+            }
+            if (n <= 2)
+            {
+                return nthFibonacci;
+            }
+
+            double prevPrev = 0;
+            double prev = 1;
+            for (int i = 1; i < n; i++)
+            {
+                nthFibonacci = prevPrev + prev;
+                prevPrev = prev;
+                prev = nthFibonacci;
+            }
+            return nthFibonacci;
+        }
+    }
+}
---
+++ week-04/day-03/Fibonacci/FibonacciTest/FiboTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fibonacci;
+using NUnit.Framework;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class FiboTest
+    {
+        [Test]
+        public void FiboSolve()
+        {
+            Fibo fibona = new Fibo();
+            int output = 21;
+
+
+            Assert.AreEqual(output, fibona.GetNthFibonacciWithFor(8));
+        }
+    }
+}
