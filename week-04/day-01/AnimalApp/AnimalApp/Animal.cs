678c8eb04af07027260ddf153ce9d01d04281ecf
Balazs Bognar <bali.bognar@outlook.com>
2017-10-05 17:26:44 +0200

    Dice created, Animal Base is done

---
+++ week-04/day-01/AnimalApp/AnimalApp/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalApp
+{
+    class Animal
+    {
+        protected int hunger = 50;
+        protected int thirst = 50;
+
+        protected void Eat()
+        {
+            hunger--;
+        }
+        protected void Drink()
+        {
+            thirst--;
+        }
+        protected void Play()
+        {
+            hunger++;
+            thirst++;
+        }
+    }
+}
---
+++ week-04/day-01/AnimalApp/AnimalApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateFarm();
+            Console.WriteLine();
+        }
+
+        static void CreateFarm()
+        {
+            Animal horse = new Animal();
+            Animal duck = new Animal();
+            Animal pig = new Animal();
+        }
+    }
+}
