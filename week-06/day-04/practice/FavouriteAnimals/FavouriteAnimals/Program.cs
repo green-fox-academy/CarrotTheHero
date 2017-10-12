e1d2c3ac024dfc706477ad8268d93db50226f754
Balazs Bognar <bali.bognar@outlook.com>
2017-10-12 12:08:39 +0200

    Animals done, and works well

---
+++ week-06/day-04/practice/FavouriteAnimals/FavouriteAnimals/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+
+namespace FavouriteAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            if (args.Length == 0)
+            {
+                Console.WriteLine("```C# FavouriteAnimals [animal] [animal]```");
+            }
+            else if (args.Length > 0)
+            {
+                string text = File.ReadAllText("favourites.txt");
+                using (StreamWriter writer = File.AppendText("favourites.txt"))
+                {
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        if (!text.Contains(args[i]))
+                        {
+                            writer.WriteLine(args[i]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
