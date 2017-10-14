2e0a59e34574e5ec41638bc08d1404326c294a78
Balazs Bognar <bali.bognar@outlook.com>
2017-10-14 13:25:23 +0200

    Creating Vil's CandyShop, but Selling Loli not works perfectly,and it is pretty wierd for me

---
+++ week-06/day-05/CandyShop2.0/CandyShop2.0/Candy.cs
@@ -0,0 +1,18 @@
+﻿using CandyShop2._0;
+
+namespace CandyShopApp
+{
+    public class Candy : Sweets
+    {
+        public Candy()
+        {
+            SugarNeeded = 10;
+            Price = 20;
+        }
+        public Candy(double percentage)
+        {
+            SugarNeeded = 10;
+            Price = (percentage / 100 + 1) * 20;
+        }
+    }
+}
---
+++ week-06/day-05/CandyShop2.0/CandyShop2.0/Lollipop.cs
@@ -0,0 +1,18 @@
+﻿using CandyShop2._0;
+
+namespace CandyShopApp
+{
+    public class Lollipop : Sweets
+    {
+        public Lollipop()
+        {
+            SugarNeeded = 5;
+            Price = 10;
+        }
+        public Lollipop(double percentage)
+        {
+            SugarNeeded = 5;
+            Price = (percentage / 100 + 1) * 10;
+        }
+    }
+}
