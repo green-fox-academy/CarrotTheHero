ca2a9bf15ec371a843adbf1f680df2420a35ffc6
Balazs Bognar <bali.bognar@outlook.com>
2017-10-17 14:06:50 +0200

    Greenfox clone done

---
+++ week-06_2/day-02/GreenFox/GreenFox/PallidaClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class PallidaClass
+    {
+        private string className;
+        private List<Student> students;
+        private List<Mentor> mentors;
+
+        public PallidaClass(string className)
+        {
+            this.className = className;
+            students = new List<Student>();
+            mentors = new List<Mentor>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        public void AddMentor(Mentor mentor)
+        {
+            mentors.Add(mentor);
+        }
+        public void Info()
+        {
+            Console.WriteLine("Pallida {0} class has {1} students and {2} mentors.", className, students.Count, mentors.Count);
+        }
+    }
+
+}
---
+++ week-06_2/day-02/GreenFox/GreenFox/Sponsor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenFox
+{
+    class Sponsor : Person
+    {
+        private string company;
+        private int hiredStudent;
+
+        public Sponsor(string name, int age, string gender, string company)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            this.company = company;
+            hiredStudent = 0;
+        }
+        public Sponsor()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+            company = "Google";
+            hiredStudent = 0;
+        }
+
+        public override void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} who represents {3} and hired {4} students so far.", name, age, gender, company, hiredStudent);
+        }
+        public void Hire()
+        {
+            hiredStudent++;
+        }
+        public override void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+    }
+}
