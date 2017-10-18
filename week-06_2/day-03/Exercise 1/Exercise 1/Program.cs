c1e549d8f978dd7fe7beb5d6e05db2725f4bb48c
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 11:23:11 +0200

    Exercise 1 completed

---
+++ week-06_2/day-03/Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            IEnumerable<int> evenNumber = n.Where(y => y % 2 == 0);
+
+            foreach (int evenNum in evenNumber)
+            {
+                Console.WriteLine(evenNum);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
