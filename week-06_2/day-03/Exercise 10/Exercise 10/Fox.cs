27f6178d5b467dd71745e623ed422f69c5aaee8d
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 17:31:43 +0200

    Exercise 10 FoxClass done, but have to work on a lot!

---
+++ week-06_2/day-03/Exercise 10/Exercise 10/Fox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Fox
+    {
+        private string color;
+        private string name;
+        private string type;
+
+        public Fox(string name, string type, string color)
+        {
+            this.name = name;
+            this.color = color;
+            this.type = type;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+    }
+}
---
+++ week-06_2/day-03/Exercise 10/Exercise 10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foxList = new List<Fox>();
+            var fox1 = new Fox("bob", "oracle", "white");
+            var fox2 = new Fox("george", "pallida", "green");
+            var fox3 = new Fox("carl", "pallida", "yellow");
+            var fox4 = new Fox("homer", "alpaga", "green");
+            var fox5 = new Fox("hugh", "alpaga", "black");
+            foxList.Add(fox1);
+            foxList.Add(fox2);
+            foxList.Add(fox3);
+            foxList.Add(fox4);
+            foxList.Add(fox5);
+
+            FindTheGreenFox(foxList);
+            Console.ReadLine();
+        }
+
+        private static void FindTheGreenFox(List<Fox> foxList)
+        {
+            var listOfFoxesQuery = from fox in foxList
+                                   where fox.Color == "green"
+                                   select fox;
+            foreach (var fox in foxList)
+            {
+                Console.WriteLine(fox);
+            }
+        }
+    }
+}
