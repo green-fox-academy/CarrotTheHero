986c0dce0c9b6138d843aba097c25fc13a9445aa
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 16:59:42 +0200

    Exercise 8 completed

---
+++ week-06_2/day-03/Exercise 8/Exercise 8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Ez A Mondat Lesz Most Az Input";
+
+            var upperCaseQuery = from character in input
+                                 where Char.IsUpper(character)
+                                 select character;
+
+            foreach (var upperCase in upperCaseQuery)
+            {
+                Console.WriteLine(upperCase);
+            }
+            Console.ReadLine();
+        }
+    }
+}
