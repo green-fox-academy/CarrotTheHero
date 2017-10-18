a5a9d72d9366b47b5c102fed4ea5a094bf290f25
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 17:06:35 +0200

    Exercise 9 completed

---
+++ week-06_2/day-03/Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = new char[] { 'k', 'a', 'k', 'a', 'o' };
+
+            var letterToWord = String.Join("", letters.Select(letter => letter.ToString()));
+            Console.WriteLine(letterToWord);
+            Console.ReadLine();
+        }
+    }
+}
