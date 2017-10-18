394659ab71ede83f817093d1437185cfde34a2ee
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 15:03:42 +0200

    Exercise 6 completed

---
+++ week-06_2/day-03/Exercise 6/Exercise 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "chilisbab";
+
+            var findTheNumberofLetters = input.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var letters in findTheNumberofLetters)
+            {
+                Console.WriteLine(letters);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
