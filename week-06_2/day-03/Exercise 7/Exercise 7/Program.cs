17adc1631ff92c60edbc31644f745b80babe18ee
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 15:10:19 +0200

    Exercise 7 completed

---
+++ week-06_2/day-03/Exercise 7/Exercise 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            IEnumerable<string> cityQuery =
+                    from city in cities
+                    where city.StartsWith("A") && city.EndsWith("I")
+                    select city;
+
+            foreach (string city in cityQuery)
+            {
+                Console.WriteLine(city);
+            }
+            Console.ReadLine();
+        }
+    }
+}
