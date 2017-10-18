2d167b26efb186dce1424bd4d5cc0368f2c9185c
Balazs Bognar <bali.bognar@outlook.com>
2017-10-18 14:58:56 +0200

    Exercise 5 completed

---
+++ week-06_2/day-03/Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var findTheNumberofNumbers = n.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var numbers in findTheNumberofNumbers)
+            {
+                Console.WriteLine(numbers);
+            }
+            Console.ReadLine();
+        }
+    }
+}
