4c33cef4095382375643d72404415d8f2ca62d18
Balazs Bognar <bali.bognar@outlook.com>
2017-10-20 14:02:53 +0200

    Exercise 3 rework with 2 more solutions

--- week-06_2/day-03/Exercise 3/Exercise 3/Program.cs
+++ week-06_2/day-03/Exercise 3/Exercise 3/Program.cs
@@ -12,13 +12,29 @@ namespace Exercise_3
         {
             int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
-            IEnumerable<int> scoreQuery =
+            var numberQuery =
                     from number in n
                     where number * number > 20
                     select number;
-            foreach (int square in scoreQuery)
+            foreach (int square in numberQuery)
             {
                 Console.WriteLine(square);
+            }
+            Console.ReadLine();
+
+            var numberQueryWithLambda = n.Where(x => (x * x) > 20);
+            foreach (int square in numberQueryWithLambda)
+            {
+                Console.WriteLine(square);
+            }
+            Console.ReadLine();
+
+            foreach (int number in n)
+            {
+                if (number * number > 20)
+                {
+                    Console.WriteLine(number);
+                }
             }
             Console.ReadLine();
         }
