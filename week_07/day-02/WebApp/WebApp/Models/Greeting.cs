0e3ab15ccdf11e67635f4a6890b38d21fc7241a7
Balazs Bognar <bali.bognar@outlook.com>
2017-10-24 14:35:16 +0200

    hello world REST done

---
+++ week_07/day-02/WebApp/WebApp/Models/Greeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Greeting
+    {
+        public long Id;
+        public string Content;
+
+        public Greeting(long id, string content)
+        {
+            Id = id;
+            Content = content;
+        }
+    }
+    
+}
