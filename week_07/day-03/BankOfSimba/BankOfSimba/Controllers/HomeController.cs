20cfb5a46103b91c1f4dbf89994f611242052f8f
Balazs Bognar <bali.bognar@outlook.com>
2017-10-25 14:26:54 +0200

    Lion King Part 3 done

--- week_07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
+++ week_07/day-03/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -18,5 +18,19 @@ namespace BankOfSimba.Controllers
             BankAccount bankAccount = new BankAccount("Simba", 2000, Animal.Lion);
             return View(bankAccount);
         }
+
+        [Route("List")]
+        public IActionResult List()
+        {
+            var ListOfNamesThaIDontKnow = new List<BankAccount>();
+
+            ListOfNamesThaIDontKnow.Add(new BankAccount("Simba", 2000, Animal.Lion));
+            ListOfNamesThaIDontKnow.Add(new BankAccount("Mufasa", 20000, Animal.Hound));
+            ListOfNamesThaIDontKnow.Add(new BankAccount("Timon", 12000, Animal.Monkey));
+            ListOfNamesThaIDontKnow.Add(new BankAccount("Pumba", 5000, Animal.MonkeyBreadTree));
+            ListOfNamesThaIDontKnow.Add(new BankAccount("Nala", 8000, Animal.Giraffe));
+
+            return View(ListOfNamesThaIDontKnow);
+        }
     }
 }
