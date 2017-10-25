81547334fbac9f7d7d0bcd38d2c7f1424d6afbfd
Balazs Bognar <bali.bognar@outlook.com>
2017-10-25 13:48:54 +0200

    Lion King Part 2 done

---
+++ week_07/day-03/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public enum Animal { Lion, Zebra, Hound, Giraffe, Monkey, MonkeyBreadTree}
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public Animal AnimalType { get; set; }
+
+        public BankAccount (string name, double balance, Animal animaltype)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animaltype;
+        }
+    }
+}
