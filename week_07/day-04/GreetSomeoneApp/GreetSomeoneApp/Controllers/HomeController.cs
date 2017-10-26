ca0eab3f91c7608aeff54d4b2edaf0f0b83c8a81
Balazs Bognar <bali.bognar@outlook.com>
2017-10-26 15:47:39 +0200

    Day 4 Greeting App working well, form is hard to understand yet

---
+++ week_07/day-04/GreetSomeoneApp/GreetSomeoneApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GreetSomeoneApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GreetSomeoneApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        UserInfo userInfo;
+
+        public HomeController(UserInfo userInfo)
+        {
+            this.userInfo = userInfo;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(userInfo);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Post(UserInfo userInfo)
+        {
+            return RedirectToAction("Greet", userInfo);
+        }
+
+        [HttpGet]
+        [Route("greet")]
+        public IActionResult Greet(UserInfo userInfo)
+        {
+            return View(userInfo);
+        }
+    }
+}
---
+++ week_07/day-04/GreetSomeoneApp/GreetSomeoneApp/Models/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreetSomeoneApp.Models
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+    }
+}
