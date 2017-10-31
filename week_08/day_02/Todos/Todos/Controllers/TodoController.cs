f1f45346533698d5991a650a5a8a455607b48748
Balazs Bognar <bali.bognar@outlook.com>
2017-10-31 14:24:59 +0100

    todo controller part done

---
+++ week_08/day_02/Todos/Todos/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Todos.Controllers
+{
+    public class TodoController : Controller
+    {
+        [Route("/todo")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/")]
+        [Route("/list")]
+        public IActionResult List()
+        {
+            string words = "This is my first todo";
+            return View((object)words);
+        }
+    }
+}
---
+++ week_08/day_02/Todos/Todos/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todos.Models
+{
+    public class Todo
+    {
+        public int Id;
+        public string Title;
+        public bool IsUrgent;
+        public bool IsDone;
+    }
+}
