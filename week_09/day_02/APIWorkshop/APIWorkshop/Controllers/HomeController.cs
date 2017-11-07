c3df98cd48f768df129d46f46ffcf796bbf3ec72
Balazs Bognar <bali.bognar@outlook.com>
2017-11-07 15:54:35 +0100

    feat(APIWorkshop) DoUntil done without one error not fixed

--- week_09/day_02/APIWorkshop/APIWorkshop/Controllers/HomeController.cs
+++ week_09/day_02/APIWorkshop/APIWorkshop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using APIWorkshop.Models;
 
 namespace APIWorkshop.Controllers
 {
@@ -55,6 +56,35 @@ namespace APIWorkshop.Controllers
         public IActionResult Append()
         {
             return NotFound();
+        }
+
+        [HttpPost]
+        [Route("/dountil/{what}")]
+        [Route("/dountil/")]
+        public IActionResult DoUntil([FromBody] JsonObject jsonObject, string what)
+        {
+            if (what == "factor")
+            {
+                int factorial = 1;
+                for (int i = 1; i < jsonObject.Until + 1; i++)
+                {
+                    factorial *= i;
+                }
+                return Json(new { result = factorial });
+            }
+            else if (what == "sum")
+            {
+                int sum = 0;
+                for (int i = 1; i < jsonObject.Until + 1; i++)
+                {
+                    sum += i;
+                }
+                return Json(new { result = sum });
+            }
+            else
+            {
+                return Json(new { error = "Please provide a number!" });
+            }
         }
     }
 }
