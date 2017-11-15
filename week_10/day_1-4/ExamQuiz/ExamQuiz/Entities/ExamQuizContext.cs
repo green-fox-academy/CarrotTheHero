061f20f7d3bbcedf3f14b18e76c12d362da62aed
Balazs Bognar <bali.bognar@outlook.com>
2017-11-15 13:53:17 +0100

    feat(ExamQuiz) created base app and json database connection

---
+++ week_10/day_1-4/ExamQuiz/ExamQuiz/Entities/ExamQuizContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamQuiz.Models;
+
+namespace ExamQuiz.Entities
+{
+    public class ExamQuizContext : DbContext
+    {
+        public ExamQuizContext(DbContextOptions<ExamQuizContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Quiz> Quizes { get; set; }
+    }
+}
---
+++ week_10/day_1-4/ExamQuiz/ExamQuiz/Migrations/20171115125215_CreatingDatabase.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ExamQuiz.Entities;
+
+namespace ExamQuiz.Migrations
+{
+    [DbContext(typeof(ExamQuizContext))]
+    [Migration("20171115125215_CreatingDatabase")]
+    partial class CreatingDatabase
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.4")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ExamQuiz.Models.Quiz", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Answer");
+
+                    b.Property<string>("Question");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Quizes");
+                });
+        }
+    }
+}
