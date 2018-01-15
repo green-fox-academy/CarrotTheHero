f1b411324a6ea3151b490da130908cdc587e5fd6
CarrotTheHero <bali.bognar@outlook.com>
2018-01-15 09:48:45 +0100

    feat(Split): create the task

---
+++ FinalExam/Split/Split/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Splitter();
+            Console.ReadLine();
+        }
+
+        private static void Splitter()
+        {
+            string word = "asd,g,hj,qw,e,wr";
+            List<string> Splitted = word.Split(',').ToList();
+
+            foreach (var part in Splitted)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+}
